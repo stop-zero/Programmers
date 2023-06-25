7c7374738d66a1a337591004535cbe7d7ecafef0
jiyoung jung <87810336+stop-zero@users.noreply.github.com>
2023-06-25 18:20:24 +0900

    [Bronze IV] Title: 특별한 날, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,14 @@
+m = int(input())
+d = int(input())
+
+if m < 2:
+    print("Before")    
+elif m == 2:
+    if d == 18: 
+        print("Special")
+    elif d > 18: 
+        print("After")
+    else: 
+        print("Before")        
+else: 
+    print("After")
