b8e20da94e512ff5807112f2d509582024efd806
jiyoung jung <87810336+stop-zero@users.noreply.github.com>
2023-06-29 22:55:41 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,16 +1,9 @@
-# N개의 바구니 M번 공을 바꿀 거 보자마자 든 생각 tmp?
-N, M = map(int, input().split())
-b = [i for i in range(1, N + 1)]
+N, M = map(int, input().split()) 
+balls = [i for i in range(N+1)]  
 
 for _ in range(M):
-    i, j = map(int, input().split())
-    tmp = b[i - 1]
-    b[i - 1] = b[j - 1]
-    b[j - 1] = tmp
- #   b[i], b[j] = b[j], b[i]
+    i, j = map(int, input().split())  
+    balls[i], balls[j] = balls[j], balls[i]  
 
-#result = ' '.join(map(str, b[1:])) #1~N번까지 번호를 문자열로 변환
-#print(result) # 런타임 에러남 왜인지..
-
-for x in b:
-     print(x, end=" ")
+result = ' '.join(map(str, balls[1:])) 
+print(result)  
