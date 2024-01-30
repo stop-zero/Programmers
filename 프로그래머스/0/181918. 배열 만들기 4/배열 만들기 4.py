6c72b3e47d9634797da446e7dc63ac244b531ab3
jiyoung jung <87810336+stop-zero@users.noreply.github.com>
2024-01-30 20:31:26 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.97 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    stk = [] 
+    i = 0  
+
+    while i < len(arr):
+        if not stk or stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        else:
+            stk.pop()
+
+    return stk
