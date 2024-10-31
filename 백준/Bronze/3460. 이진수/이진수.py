8910f2d1e69a8e3106959dc0911873f7e262e882
jiyoung jung <87810336+stop-zero@users.noreply.github.com>
2024-10-31 19:47:50 +0900

    [Bronze III] Title: 이진수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.py
+++ 백준/Bronze/3460. 이진수/이진수.py
@@ -1,14 +1,14 @@
 T = int(input())
 
 for _ in range(T):
-    n = int(input()) 
-    positions = [] 
-
-    index = 0
+    n = int(input())
+    position = 0
+    positions = []
+    
     while n > 0:
         if n % 2 == 1:
-            positions.append(index)
-        n //= 2
-        index += 1
+            positions.append(position)
+        n = n // 2
+        position += 1
     
-    print(' '.join(map(str, positions)))  
+    print(" ".join(map(str, positions)))
